37fe94c7367eee6eb14c53ee75a8681b40b1d7c6
yuys13 <yuys13@users.noreply.github.com>
2024-10-06 09:34:22 +0900

    Add tape files for demo

---
+++ vhs/init.lua
@@ -0,0 +1,43 @@
+vim.opt.list = true
+
+vim.opt.listchars = {
+  tab = '¦-',
+  trail = '-',
+  nbsp = '+',
+  extends = '»',
+  precedes = '«',
+}
+
+vim.opt.number = true
+
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/plenary.nvim")
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/collama.nvim")
+
+local config = {
+	base_url = "http://localhost:11434/api/",
+	fim = {
+		model = "codellama:7b-code",
+		tokens = require("collama.preset.tokens").codellama,
+	},
+}
+
+local augroup = vim.api.nvim_create_augroup("my_collama_augroup", { clear = true })
+
+-- auto execute debounced_request
+vim.api.nvim_create_autocmd({ "InsertEnter", "CursorMovedI", "TextChangedI" }, {
+	group = augroup,
+	callback = function()
+		require("collama.copilot").debounced_request(config, 1000)
+	end,
+})
+-- auto cancel
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+	group = augroup,
+	callback = function()
+		require("collama.copilot").clear()
+	end,
+})
+-- map accept key
+vim.keymap.set("i", "<C-f>", require("collama.copilot").accept)
+
+require("collama.logger").setup(vim.notify)
